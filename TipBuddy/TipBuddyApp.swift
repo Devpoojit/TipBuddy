3d02cdbd7b559f8566079fcb2b252534e7dc2abc
Devpoojit Verma <devpoojit10912@gmail.com>
2025-04-25 22:02:21 +0530

    Initial Commit

---
+++ TipBuddy/TipBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipBuddyApp.swift
+//  TipBuddy
+//
+//  Created by Devpoojit Verma on 25/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TipBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
